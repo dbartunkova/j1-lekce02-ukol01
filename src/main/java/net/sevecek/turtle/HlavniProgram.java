b18f85b4ac8e82e76269c8531318526a9475f565
Dana Bartunkova <DanaFicalova@gmail.com>
2022-04-16 20:49:02 +0200

    Metoda nakresliD(), nakresliA().

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -10,17 +10,66 @@ public class HlavniProgram {
         zofka = new Turtle();
 
 
-        nakresliKolecko(zofka);
+        pismenoA(zofka);
 
-        zofka.turnRight(60.0);
-        zofka.move(160.0);
+        //nakresliD(zofka);
 
-        nakresliOsmiuhelnik(zofka);
+
+        //nakresliKolecko(zofka);
+        //for (int i = 0; i < 3; i++) {
+            //nakresliPaprsekSlunce(zofka);
+
+
+
+
+        //nakresliPaprsekSlunce(zofka);
+
+
+    }
+
+    private void pismenoA(Turtle zofka) {
+        zofka.penDown();
+        zofka.turnRight(30.0);
+        zofka.move(150.0);
+        zofka.turnRight(120.0);
+        zofka.move(150.0);
+        zofka.penUp();
+        zofka.move(-50.0);
+        zofka.penDown();
+        zofka.turnRight(120.0);
+        zofka.move(100.0);
+        zofka.penUp();
+    }
+
+    private void nakresliD(Turtle zofka) {
+        zofka.move(110);
+        zofka.turnRight(80.0);
+        for (int i = 0; i <39 ; i++) {
+            zofka.turnRight(5.0);
+            zofka.move(5.0);
+        }
+        zofka.penUp();
+    }
+
+    private void nakresliPaprsekSlunce(Turtle zofka) {
+        zofka.turnLeft(90.0);
+        zofka.move(30.0);
+        zofka.move(-30.0);
+        zofka.penUp();
+    }
+
+
+    //nakresliKolecko(zofka);
+
+        //zofka.turnRight(60.0);
+        //zofka.move(160.0);
+
+        //nakresliOsmiuhelnik(zofka);
 
         //nakresliDomecek(zofka);
 
         //nakresliPrasatko(zofka);
-    }
+
 
     private void nakresliKolecko(Turtle zofka) {
         for (int i = 0; i < 75; i++) {
@@ -36,7 +85,7 @@ public class HlavniProgram {
             zofka.penDown();
             zofka.move(50.0);
             zofka.turnRight(45.0);
-            zofka.penUp();
+           // zofka.penUp();
         }
     }
 
