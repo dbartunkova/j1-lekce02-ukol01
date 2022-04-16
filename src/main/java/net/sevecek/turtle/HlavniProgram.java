eaae602d6c6a4b5d4057ed60547da713985b255b
Dana Bartunkova <DanaFicalova@gmail.com>
2022-04-16 17:45:15 +0200

    Metoda nakresliPrasatko().

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,51 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        nakresliPrasatko(zofka);
     }
 
+    private void nakresliPrasatko(Turtle zofka) {
+        zofka.turnLeft(45.0);
+        zofka.move(100.0);
+        zofka.turnRight(90.0);
+        zofka.move(100.0);
+        zofka.turnRight(135.0);
+        zofka.move(145.0);
+        zofka.turnLeft(90.0);
+        zofka.move(200.0);
+        zofka.turnLeft(90.0);
+        zofka.move(145.0);
+        zofka.turnLeft(90.0);
+        zofka.move(200.0);
+        zofka.turnLeft(90.0);
+        zofka.move(145.0);
+
+        zofka.turnRight(30.0);
+        zofka.move(40.0);
+
+        zofka.penUp();
+        zofka.move(-40.0);
+
+        zofka.penDown();
+        zofka.turnLeft(60);
+        zofka.move(40.0);
+
+        zofka.penUp();
+        zofka.move(-40.0);
+
+        zofka.penDown();
+        zofka.turnLeft(60);
+        zofka.move(200.0);
+
+        zofka.turnRight(60.0);
+        zofka.move(40.0);
+
+        zofka.penUp();
+        zofka.move(-40.0);
+
+        zofka.penDown();
+        zofka.turnRight(60.0);
+        zofka.move(40.0);
+    }
 }
